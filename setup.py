06b08f0cc2fddc0b3bf94854ac11bc7abbf214e2
D3-4D <bernardouss0ps@gmail.com>
2025-04-04 12:34:51 -0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="DeepScrape",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        "selenium",
+        "webdriver-manager"
+    ],
+    author="D3-4D",
+    description="Allows for free DeepAI API usage through web scraping with Selenium.",
+    license="MIT",
+    url="https://github.com/D3-4D/DeepScrape",
+)
